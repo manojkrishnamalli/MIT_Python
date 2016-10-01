e4e91aa77e7f75ed3d75d5a215eb6a97644aace2
Manoj Krishna Malli <manojkrishnamalli@gmail.com>
2016-10-02 01:32:34 +0530

    More Programs for For Loop

---
+++ forLoop3.py
@@ -0,0 +1,7 @@
+counter = 0
+summing = 12
+print('Goodbye!')
+for counter in range(5) :
+    summing -= 2
+    print(summing)  
+
---
+++ forLoop4.py
@@ -0,0 +1,7 @@
+end = 6
+num = 0
+summing = 0
+for num in range(end) :
+    num += 1
+    summing += num
+print(summing)
